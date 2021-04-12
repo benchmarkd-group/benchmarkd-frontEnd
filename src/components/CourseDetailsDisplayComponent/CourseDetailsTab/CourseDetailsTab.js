2f55ae75740ea2498607d4887627d7ae18602898
I-Curious <simith_b@yahoo.in>
2021-04-12 11:23:39 +0530

    course details changes'

--- src/components/CourseDetailsDisplayComponent/CourseDetailsTab/CourseDetailsTab.js
+++ src/components/CourseDetailsDisplayComponent/CourseDetailsTab/CourseDetailsTab.js
@@ -34,36 +34,235 @@ class CourseDetailsTab extends Component {
   
   <div>
     
-    <div className="tab" onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
-      {this.state.data.map((button, i) => (
-        
-          <button data-tip data-for="registerTip" className="tablinks" id ={button.id} onClick={() => this.handleClick(i)} >
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+
+      <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Exams
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Compare Courses</a>
+      </li>
+    </ul>
+    </div>
+    </nav>
+    <div className="tab" > 
+          
             <div className='tab-label-container'>
-              <FontAwesomeIcon icon={button.icon} />
-              <span id='tabLabelName' className='tab-name-contracted'>{button.name}</span>
-            </div>
-          </button> 
+            <ul id="nav" class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                  <a class="page-scroll" href="#home">Home</a>
+              </li>
+              <li class="nav-item">
+                  <a class="page-scroll" href="#features">Features</a>
+              </li>
+              <li class="nav-item">
+                  <a class="page-scroll" href="#about">About</a>
+              </li>
+              <li class="nav-item">
+                  <a class="page-scroll" href="#facts">Why</a>
+              </li>
+              <li class="nav-item">
+                  <a class="page-scroll" href="#team">Team</a>
+              </li>
+              <li class="nav-item">
+                  <a class="page-scroll" href="#blog">Blog</a>
+              </li>
+          </ul>
+               </div>
         
-      )
-        )
-      }
-    </div>
+            
   
-    <div className="tabcontent">
-      {this.state.currentTab !==1 &&
-        <React.Fragment>
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-          </nav>
-        </React.Fragment>
-      }
-      {this.state.currentTab !== -1 &&
-        <React.Fragment>
-          <h3>{this.state.data[this.state.currentTab].name}</h3>
-          <p>{this.state.data[this.state.currentTab].info}</p>
-        </React.Fragment>
-      }
+ 
     </div>
+    <div id="home" class="header-hero bg_cover" >
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="header-hero-content text-center">
+                            <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Basic - SaaS Landing Page</h3>
+                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">Kickstart Your SaaS or App Site</h2>
+                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+                            <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a>
+                        </div> 
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-hero-image text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="1.4s">
+                            <img src="assets/images/header-hero.png" alt="hero"></img>
+                        </div> 
+                    </div>
+                </div> 
+            </div> 
+            <div id="particles-1" class="particles"></div>
+        </div> 
+        <div id="home" class="header-hero bg_cover" >
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="header-hero-content text-center">
+                            <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Basic - SaaS Landing Page</h3>
+                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">Kickstart Your SaaS or App Site</h2>
+                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+                            <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a>
+                        </div> 
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-hero-image text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="1.4s">
+                            <img src="assets/images/header-hero.png" alt="hero"></img>
+                        </div> 
+                    </div>
+                </div> 
+            </div> 
+            <div id="particles-1" class="particles"></div>
+        </div> 
+        <div id="home" class="header-hero bg_cover" >
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="header-hero-content text-center">
+                            <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Basic - SaaS Landing Page</h3>
+                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">Kickstart Your SaaS or App Site</h2>
+                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+                            <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a>
+                        </div> 
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-hero-image text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="1.4s">
+                            <img src="assets/images/header-hero.png" alt="hero"></img>
+                        </div> 
+                    </div>
+                </div> 
+            </div> 
+            <div id="particles-1" class="particles"></div>
+        </div> 
+        <div id="home" class="header-hero bg_cover" >
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="header-hero-content text-center">
+                            <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Basic - SaaS Landing Page</h3>
+                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">Kickstart Your SaaS or App Site</h2>
+                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+                            <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a>
+                        </div> 
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-hero-image text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="1.4s">
+                            <img src="assets/images/header-hero.png" alt="hero"></img>
+                        </div> 
+                    </div>
+                </div> 
+            </div> 
+            <div id="particles-1" class="particles"></div>
+        </div><div id="home" class="header-hero bg_cover" >
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="header-hero-content text-center">
+                            <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Basic - SaaS Landing Page</h3>
+                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">Kickstart Your SaaS or App Site</h2>
+                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+                            <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a>
+                        </div> 
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-hero-image text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="1.4s">
+                            <img src="assets/images/header-hero.png" alt="hero"></img>
+                        </div> 
+                    </div>
+                </div> 
+            </div> 
+            <div id="particles-1" class="particles"></div>
+        </div><div id="home" class="header-hero bg_cover" >
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="header-hero-content text-center">
+                            <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Basic - SaaS Landing Page</h3>
+                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">Kickstart Your SaaS or App Site</h2>
+                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+                            <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a>
+                        </div> 
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-hero-image text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="1.4s">
+                            <img src="assets/images/header-hero.png" alt="hero"></img>
+                        </div> 
+                    </div>
+                </div> 
+            </div> 
+            <div id="particles-1" class="particles"></div>
+        </div><div id="home" class="header-hero bg_cover" >
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="header-hero-content text-center">
+                            <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Basic - SaaS Landing Page</h3>
+                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">Kickstart Your SaaS or App Site</h2>
+                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+                            <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a>
+                        </div> 
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-hero-image text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="1.4s">
+                            <img src="assets/images/header-hero.png" alt="hero"></img>
+                        </div> 
+                    </div>
+                </div> 
+            </div> 
+            <div id="particles-1" class="particles"></div>
+        </div>
+        <div id="features" class="header-hero bg_cover" >
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="header-hero-content text-center">
+                            <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Basic - SaaS Landing Page</h3>
+                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">Kickstart Your SaaS or App Site</h2>
+                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+                            <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a>
+                        </div> 
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-hero-image text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="1.4s">
+                            <img src="assets/images/header-hero.png" alt="hero"></img>
+                        </div> 
+                    </div>
+                </div> 
+            </div> 
+            <div id="particles-1" class="particles"></div>
+        </div> 
   </div>
     )
     }
